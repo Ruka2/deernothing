d565145069f505459c241c35d28a50174cc24045
deernothing <893730777@qq.com>
2020-02-28 21:35:02 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});particlesJS("");
+document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});particlesJS("");})
